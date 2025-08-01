0b5d3d93b0c59c6ffd08e6aa6a9146af73b7a938
Markus Walther <mark@saiko.tech>
2025-08-01 11:05:25 +0100

    Revert "fix"
    This reverts commit 838555b4eb162c16e294e623ac26d65f1ea9fcc6.

--- src/core/managers/entity_manager.h
+++ src/core/managers/entity_manager.h
@@ -197,7 +197,7 @@ struct EmitSound_t
     bool m_bWarnOnMissingCloseCaption;
     bool m_bWarnOnDirectWaveReference;
     CEntityIndex m_nSpeakerEntity;
-    CUtlVectorMemory<Vector, CUtlVectorMemory<Vector, int>> m_UtlVecSoundOrigin;
+    CUtlVector<Vector, CUtlMemory<Vector, int>> m_UtlVecSoundOrigin;
     SoundEventGuid_t m_nForceGuid;
     gender_t m_SpeakerGender;
 };
